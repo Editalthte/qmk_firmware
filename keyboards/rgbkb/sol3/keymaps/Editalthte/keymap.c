209002c268e447a04da5c70237796c885296a120
Editalthte <jordan.klinefelter+github@protonmail.com>
2022-09-17 11:13:32 -0400

    - Refactor of defines to enums - Removed / Renamed various default layers and keycodes

--- keyboards/rgbkb/sol3/keymaps/Editalthte/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/Editalthte/keymap.c
@@ -7,85 +7,57 @@
  * ----------------------------------------------------------------------------
  */
 
+#include "keycode.h"
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
-/* Qwerty
- * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┐  ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┐
- * │ GESC │   1  │   2  │   3  │   4  │   5  │   -  │  │   =  │   6  │   7  │   8  │   9  │   0  │ BkSp │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Tab  │   Q  │   W  │   E  │   R  │   T  │   [  │  │   ]  │   Y  │   U  │   I  │   O  │   P  │   \  │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │FNCAPS│   A  │   S  │   D  │   F  │   G  │   (  │  │   )  │   H  │   J  │   K  │   L  │   ;  │   '  │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │Shift │   Z  │   X  │   C  │   V  │   B  │   {  │  │   }  │   N  │   M  │   ,  │   .  │   /  │Enter │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Ctrl │  Win │  Alt │  RGB │ ADJ  │      │ DEL  │  │ Enter│      │  FN  │ Left │ Down │ Up   │Right │
- * └──────┴──────┴──────┴──────┴──────┤ Space├──────┤  ├──────┤ Space├──────┴──────┴──────┴──────┴──────┘
- *                                    │      │ DEL  │  │ Enter│      │
- *                                    └──────┴──────┘  └──────┴──────┘
- * ┌──────┬──────┬──────┬──────┬──────┬──────┐                ┌──────┬──────┬──────┬──────┬──────┬──────┐
- * │Vol Dn│Vol Up│Vol Dn│Vol Up│Vol Dn│Vol Up│                │Vol Dn│Vol Up│Vol Dn│Vol Up│Vol Dn│Vol Up│
- * └──────┴──────┴──────┴──────┴──────┴──────┘                └──────┴──────┴──────┴──────┴──────┴──────┘
- * ┌──────┬──────┬──────┬──────┬──────┐                              ┌──────┬──────┬──────┬──────┬──────┐
- * │Vol Dn│Vol Up│ Prev │ Play │ Next │                              │Vol Dn│Vol Up│ Prev │ Play │ Next │
- * └──────┴──────┴──────┴──────┴──────┘                              └──────┴──────┴──────┴──────┴──────┘
- */
-
-// Sol Layers
 enum sol_layers
 {
-	_QWERTY,
-	_FN,
-	_ADJUST
+	MAIN_LAYER,
+	FN_LAYER,
+	DOWNSHIFT_LAYER,
+	NUMPAD_LAYER,
+	NAVIGATION_LAYER,
+	SOL_CONTROL_LAYER
 };
 
-// Sol Keycodes
 enum sol_keycodes
 {
-	// Disables touch processing
-	TCH_TOG = SAFE_RANGE,
+	TCH_TOG = SAFE_RANGE,  // Disables touch processing
 	MENU_BTN,
 	MENU_UP,
 	MENU_DN,
-	RGB_RST
-};
+	RGB_RST,
 
-#define QWERTY   DF(_QWERTY)
-#define FN       MO(_FN)
-#define ADJUST   MO(_ADJUST)
-#define FN_CAPS  LT(_FN, KC_CAPS)
-//#define RGB_ADJ  LT(_ADJUST, RGB_TOG)
+	// Momentary Layer Switch
+	FN_LAYER_KEY = MO(FN_LAYER),
+	DOWNSHIFT_LAYER_KEY = MO(DOWNSHIFT_LAYER),
+	NUMPAD_LAYER_KEY = MO(NUMPAD_LAYER),
+	NAVIGATION_LAYER_KEY = MO(NAVIGATION_LAYER),
+
+	// Toggle Layer
+	SOL_CONTROL_LAYER_TOGGLE = TG(SOL_CONTROL_LAYER)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
-	[_QWERTY] = LAYOUT(
+	[MAIN_LAYER] = LAYOUT(
 		KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
 		KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-		FN_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                  KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+		DOWNSHIFT_LAYER_KEY,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                  KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR,                  KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-		KC_LCTL,  KC_LGUI, KC_LALT, RGB_TOG, ADJUST,  KC_SPC,  KC_PGDN, KC_DEL, KC_ENT,  KC_PGUP, KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL,
+		KC_LCTL,  KC_LGUI, KC_LALT, RGB_TOG, KC_NO,  KC_SPC,  KC_PGDN, KC_DEL, KC_ENT,  KC_PGUP, KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL,
 
 		KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU,                                     KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU,
 		KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_MPRV,                                                       KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_MPRV
 	),
 
-	[_FN] = LAYOUT(
+	[FN_LAYER] = LAYOUT(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
 		_______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______,                   _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_PGUP,
 		_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,                   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_PGDN,
 		_______,  AU_TOG,  MU_TOG,  MU_MOD, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
 		_______, CK_TOGG,   CK_UP, CK_DOWN, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,
-
-		_______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
-	),
-
-	[_ADJUST] = LAYOUT(
-		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-		_______, RGB_SAD, RGB_VAI, RGB_SAI, QK_BOOT,   _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
-		_______, RGB_HUD, RGB_VAD, RGB_HUI, RGB_RST, _______, DM_REC1,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
-		_______, RGB_SPD, _______, RGB_SPI, _______, _______, DM_RSTP,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
-		_______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY,  _______,
 
 		_______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
@@ -128,29 +100,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
 		default:
 			return true;
-	}
-}
-
-void render_layer_status(void)
-{
-	// Host Keyboard Layer Status
-	oled_write_P(PSTR("Layer"), false);
-
-	switch (get_highest_layer(layer_state))
-	{
-		case _QWERTY:
-			oled_write_ln_P(PSTR("QWRTY"), false);
-			break;
-
-		case _FN:
-			oled_write_ln_P(PSTR("FN   "), false);
-			break;
-
-		case _ADJUST:
-			oled_write_ln_P(PSTR("Adjst"), false);
-			break;
-
-		default:
-			oled_write_ln_P(PSTR("Undef"), false);
 	}
 }
