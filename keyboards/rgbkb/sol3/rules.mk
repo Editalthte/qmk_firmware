d736128628bb005713e34905118d8311977a29d3
Editalthte <jordan.klinefelter+github@protonmail.com>
2022-10-29 16:40:58 -0400

    More rules config updates

--- keyboards/rgbkb/sol3/rules.mk
+++ keyboards/rgbkb/sol3/rules.mk
@@ -11,19 +11,22 @@ SRC += common_oled.c
 QUANTUM_LIB_SRC += i2c_master.c
 
 # Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = lite      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 AUDIO_ENABLE = yes          # Audio output
 AUDIO_DRIVER = dac_additive # Audio output
+DIP_SWITCH_ENABLE = yes     # The Sol3 has dip switches, but I'm not 100% if this feature is actually needed.
+                            # Leaving it enabled until I know exactly if it's necessary (ie: it will stay enabled forever).
 
-DYNAMIC_MACRO_ENABLE = yes
-DIP_SWITCH_ENABLE = yes
+# Features that make me go "neat!", but I probably won't actually end up ever using
+MOUSEKEY_ENABLE = no        # Mouse keys
+DYNAMIC_MACRO_ENABLE = no	# Create macros ad-hoc
+
+# Features I probably won't ever use
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+
 
 # RGB Lighting
 WS2812_DRIVER = pwm
